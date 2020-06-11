cabbffe3831660b6770b84339f54073c9e0f0199
SloWax <rjsnrl0128@naver.com>
2020-06-12 03:03:33 +0900

    complete.

--- git/200529 Dominos Test - 앱 구현 git/Dominos11_Starter/Dominos11_Starter/CategoryViewController.swift
+++ git/200529 Dominos Test - 앱 구현 git/Dominos11_Starter/Dominos11_Starter/CategoryViewController.swift
@@ -19,32 +19,36 @@ class CategoryViewController: UIViewController {
     
     navigationItem.title = "Domino's"
     
+    imageLogo.frame = CGRect(origin: .zero, size: CGSize(width: view.frame.width, height: 200))
     imageLogo.image = UIImage(named: "logo")
-    imageLogo.frame = CGRect(x: 0, y: 0, width: tableView.frame.width, height: tableView.frame.height/3)
+    imageLogo.contentMode = .scaleAspectFit
     
     tableView.frame = view.frame
     tableView.rowHeight = 100
     tableView.tableHeaderView = imageLogo
     tableView.dataSource = self
-    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CellID")
+    tableView.delegate = self
+    tableView.register(CategoryCell.self, forCellReuseIdentifier: CategoryCell.identifier)
     view.addSubview(tableView)
-    
-    
-//    imageView.image = UIImage(named: "logo")
-//    tableView.addSubview(imageView)
     
   }
 }
 
-extension CategoryViewController: UITableViewDataSource {
+extension CategoryViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         category.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CellID", for: indexPath)
-        cell.imageView?.image = UIImage(named: category[indexPath.row])
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: CategoryCell.identifier, for: indexPath) as! CategoryCell
+        cell.imageCategory.image = UIImage(named: category[indexPath.row])
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let productVC = ProductViewController()
+        productVC.navigationItem.title = menuData[indexPath.row].category
+        productVC.products = menuData[indexPath.row].products
+        navigationController?.pushViewController(productVC, animated: true)
     }
 }
--- git/200529 Dominos Test - 앱 구현 git/Dominos11_Starter/Dominos11_Starter/WishListViewController.swift
+++ git/200529 Dominos Test - 앱 구현 git/Dominos11_Starter/Dominos11_Starter/WishListViewController.swift
@@ -9,28 +9,84 @@
 import UIKit
 
 class WishListViewController: UIViewController {
-    
-    let label = UILabel()
+
+    let singleton = Singleton.shared
     let tableView = UITableView()
-  
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        tableView.reloadData()
+    }
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+
     navigationItem.title = "Wish List"
-    
+
     let listClearButton = UIBarButtonItem(title: "목록 지우기", style: .plain, target: self, action: #selector(listClearToggle(_:)))
     let oderButton = UIBarButtonItem(title: "주문", style: .plain, target: self, action: #selector(oderToggle(_:)))
-    
+
     navigationItem.leftBarButtonItem = listClearButton
     navigationItem.rightBarButtonItem = oderButton
-    
-    tableView.frame = view.frame
-    
+
+    tableView.frame = view.safeAreaLayoutGuide.layoutFrame
+    tableView.dataSource = self
+    tableView.rowHeight = 100
+    tableView.allowsSelection = false
+    view.addSubview(tableView)
+
   }
-    @objc func listClearToggle(_ sender: UIButton) {
-        
+
+    @objc func listClearToggle(_ sender: Any) {
+        singleton.wishList.removeAll()
+        tableView.reloadData()
     }
+
     @objc func oderToggle(_ sender: UIButton) {
-        
+        let keys = singleton.wishList.keys.sorted()
+        var paymentList = ""
+        var paymentValue = 0
+        for key in keys {
+            for category in menuData {
+                for menu in category.products {
+                    if key == menu.name {
+                        paymentList += "\(key) - \(singleton.wishList[key] ?? 0) 개\n"
+                        paymentValue += (singleton.wishList[key]! * menu.price)
+                    }
+                }
+            }
+        }
+        paymentList += "결제금액 : \(paymentValue)원"
+        alertAction(title: "결제내역", message: paymentList)
+    }
+    
+    func alertAction(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "결제하기", style: .default, handler: listClearToggle(_:))
+        let cancleButton = UIAlertAction(title: "취소", style: .cancel)
+        alert.addAction(okButton)
+        alert.addAction(cancleButton)
+        present(alert, animated: true)
     }
 }
+extension WishListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return singleton.wishList.keys.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let keys = Singleton.shared.wishList.keys.sorted()
+        let cell: UITableViewCell
+        if let reusableCell = tableView.dequeueReusableCell(withIdentifier: "cell") {
+            cell = reusableCell
+        } else {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        }
+        cell.imageView?.image = UIImage(named: "\(keys[indexPath.row])")
+        cell.textLabel?.text = keys[indexPath.row]
+        cell.detailTextLabel?.text = "주문수량: \(singleton.wishList[keys[indexPath.row]] ?? 0) 개"
+        return cell
+    }
+}
+
