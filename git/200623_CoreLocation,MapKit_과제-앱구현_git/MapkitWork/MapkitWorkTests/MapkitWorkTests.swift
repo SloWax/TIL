a368a35038faed10d3b0414a8429bd9beec84a3b
SloWax <rjsnrl0128@naver.com>
2020-07-19 20:24:36 +0900

    complete.

---
+++ git/200623_CoreLocation,MapKit_과제-앱구현_git/MapkitWork/MapkitWorkTests/MapkitWorkTests.swift
@@ -0,0 +1,34 @@
+//
+//  MapkitWorkTests.swift
+//  MapkitWorkTests
+//
+//  Created by 표건욱 on 2020/07/16.
+//  Copyright © 2020 SloWax. All rights reserved.
+//
+
+import XCTest
+@testable import MapkitWork
+
+class MapkitWorkTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
